aeaeba780f00031b364df1cf9057194812093e0f
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-02-27 17:32:51 +0000

    implemented bubble sort

--- Sort.java
+++ Sort.java
@@ -3,51 +3,60 @@ import java.util.List;
 
 public class Sort <T> {
 
-    public static <T extends Comparable<T>> void quick(final List<T> input, final Comparator<T> compartor) {
+    public static <T extends Comparable<T>> void quick(final List<T> input, final Comparator<T> comparator) {
 
     }
 
-    public static <T extends Comparable<T>> void quick(final T[] input, final Comparator<T> compartor) {
+    public static <T extends Comparable<T>> void quick(final T[] input, final Comparator<T> comparator) {
 
     }
 
-    public static <T extends Comparable<T>> void bogo(final List<T> input, final Comparator<T> compartor) {
+    public static <T extends Comparable<T>> void bogo(final List<T> input, final Comparator<T> comparator) {
 
     }
 
-    public static <T extends Comparable<T>> void bogo(final T[] input, final Comparator<T> compartor) {
-
-    }
-
-
-    public static <T extends Comparable<T>> void insertion(final List<T> input, final Comparator<T> compartor) {
-
-    }
-
-    public static <T extends Comparable<T>> void insertion(final T[] input, final Comparator<T> compartor) {
+    public static <T extends Comparable<T>> void bogo(final T[] input, final Comparator<T> comparator) {
 
     }
 
 
-    public static <T extends Comparable<T>> void selection(final List<T> input, final Comparator<T> compartor) {
+    public static <T extends Comparable<T>> void insertion(final List<T> input, final Comparator<T> comparator) {
 
     }
 
-    public static <T extends Comparable<T>> void selection(final T[] input, final Comparator<T> compartor) {
+    public static <T extends Comparable<T>> void insertion(final T[] input, final Comparator<T> comparator) {
 
     }
 
-    public static <T extends Comparable<T>> void bubble(final List<T> input, final Comparator<T> compartor) {
-        for (int i = 0; i < input.size(); i++) {
-            if (compartor.compare(input.get(i), input.get(i+1)) < 0) {
-                T temp = input.get(i);
-                input.set(i, input.get(i+1));
-                input.set(i+1, temp);
+
+    public static <T extends Comparable<T>> void selection(final List<T> input, final Comparator<T> comparator) {
+
+    }
+
+    public static <T extends Comparable<T>> void selection(final T[] input, final Comparator<T> comparator) {
+
+    }
+
+    public static <T extends Comparable<T>> void bubble(final List<T> input, final Comparator<T> comparator) {
+        for (int i = 0; i < input.size()-1; i++) {
+            for (int j = 1; j < input.size()-i; j++) {
+                if (comparator.compare(input.get(j-1), input.get(j)) > 0) {
+                    T temp = input.remove(j-1);
+                    input.add(j, temp);
+                }
             }
         }
     }
 
-    public static <T extends Comparable<T>> void bubble(final T[] input, final Comparator<T> compartor) {
-
+    public static <T extends Comparable<T>> void bubble(final T[] input, final Comparator<T> comparator) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 1; j < input.length-i; j++) {
+                if (comparator.compare(input[j-1], input[j]) > 0) {
+                    T temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                }
+            }
+        }
     }
 }
---
+++ SortTestIntegers.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortTestIntegers {
+    Comparator<Integer> comparator = (x, y) -> x > y? 1: x < y? -1: 0;
+
+    @Test
+    void bubbleList() {
+        List<Integer> list = generateRandomList(10);
+        List<Integer> sortedList = new ArrayList<>(list);
+
+        Sort.bubble(list, comparator);
+        sortedList.sort(Integer::compareTo);
+
+        assertEquals(sortedList, list);
+    }
+
+    @Test
+    void bubbleList2() {
+        List<Integer> list = generateRandomList(1000);
+        List<Integer> sortedList = new ArrayList<>(list);
+
+        Sort.bubble(list, comparator);
+        sortedList.sort(Integer::compareTo);
+
+        assertEquals(sortedList, list);
+    }
+
+    @Test
+    void bubbleArray() {
+        Integer[] sortedList = generateRandomList(10).toArray(new Integer[10]);
+        Integer[] list = sortedList.clone();
+
+        Sort.bubble(list, comparator);
+        Arrays.sort(sortedList, comparator);
+
+        assertArrayEquals(sortedList, list);
+    }
+
+    private ArrayList<Integer> generateRandomList(int size) {
+        ArrayList<Integer> al = new ArrayList<>(size);
+        Random r = new Random();
+        for (int i = 0; i < size; i++) {
+            al.add(r.nextInt(size)); //add random number
+        }
+        return al;
+    }
+}
